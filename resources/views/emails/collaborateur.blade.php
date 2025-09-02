acc5f242e7f92007307d88ee13d407d1b1f7cd06
Noobs440 <arieldoubissi330@gmail.com>
2025-09-02 18:47:17 +0200

    petits bugs

--- resources/views/emails/collaborateur.blade.php
+++ resources/views/emails/collaborateur.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Bonjour {{ $collab->nom_collab }},
 
-Vous avez été ajouté en tant que **collaborateur** sur un projet dans la plateforme **AcadProManage**.
+Vous avez été ajouté en tant que **collaborateur** sur un projet dans la plateforme **CollabFacultyRise**.
 
 Merci de consulter votre compte pour voir les détails du projet.
 
